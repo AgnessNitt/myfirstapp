e1bf073ac564eb475d27430a30fb7e12fd02f3a4
ProfessorHulk <psyx89@gmail.com>
2022-04-29 08:29:23 +0300

    change layout

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_dogma).setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra(MainActivity2.EXTRA_TITLE, "Догма")
+            startActivityForResult(intent, 42)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 42) {
+            if (resultCode == RESULT_OK && data != null) {
+                val result = data.getBooleanExtra(MainActivity2.RESULT_FAVORITE, false)
+                Toast.makeText(this, "Результат $result", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity2.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        title = intent.getStringExtra(EXTRA_TITLE) ?: error("No title provided")
+
+        val result = Intent()
+        result.putExtra(RESULT_FAVORITE, true)
+        setResult(RESULT_OK, result)
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "title"
+        const val RESULT_FAVORITE = "favorite"
+    }
+}
