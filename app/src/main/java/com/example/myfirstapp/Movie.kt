0edd021077f13eb6b92d5a822641895d440ba335
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2022-06-04 13:27:36 +0300

    Added RecyclerView

---
+++ app/src/main/java/com/example/myfirstapp/Movie.kt
@@ -0,0 +1,10 @@
+package com.example.myfirstapp
+
+import androidx.annotation.DrawableRes
+
+data class Movie(
+    val title: String,
+    val description: String,
+    @DrawableRes
+    val imageResId: Int
+)
