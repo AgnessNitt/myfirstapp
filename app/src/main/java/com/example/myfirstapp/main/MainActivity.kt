afd85cadae7a9a41c92b9b382cd112b3bc9d9dce
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2022-07-02 23:33:46 +0300

    Replaced activity context with application context

--- app/src/main/java/com/example/myfirstapp/main/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/main/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MoviesDataSource.createMovies(this)
+        MoviesDataSource.createMovies(applicationContext)
         initRecyclerView()
         setOnShowFavoritesButtonClickListener()
     }
